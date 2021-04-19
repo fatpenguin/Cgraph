1676036cb26d194a49599c32a00a0074c2e06138
drobertaud <davidr@prevayl.com>
2021-04-19 08:44:32 +0200

    continue on list implementation

--- README.md
+++ README.md
@@ -1,5 +1,17 @@
 # Cgraph
 
-C implementation of a list Graph
+C implementation of a list Graph.
+Use the list and table C++ libraries
+
+## Graph
+
+## List
+
+list.hpp + list.cpp
+
+This module of list management est generic and independant from any application.
+It manages lists of simple elements with head lists.
+
+## Table
 
 Use the Michel Divay implementation
--- list.cpp
+++ list.cpp
@@ -1,7 +1,7 @@
 /*
 list.cpp
 This module of list management est generic and independant from any application.
-It manages lsits of simple elements with head lists.
+It manages lists of simple elements with head lists.
 */
 
 #include <stdlib.h>
@@ -9,12 +9,32 @@ It manages lsits of simple elements with head lists.
 #include "list.hpp"
 
 // Module local
-static element *create_element();
+static element_t *create_element();
 static void insert_after(list *li, element_t *previous, Object *object);
 
 static Object *extract_after(list_t *li, element_t *previous);
-static element *current_element(list_t *li);
+static element_t *current_element(list_t *li);
 static boolean in_order(Object *object1, Object *object2, boolean ascending_order, int (*compare)(Object *, Object *));
+
+boolean remove_object(list_t *li, Object *object)
+{
+    element_t *previous = NULL;
+    element_t *ptc = NULL;
+
+    boolean found = false;
+    open_list(li);
+    while (!end_list(li) && !found)
+    {
+        previous = ptc;
+        ptc = current_element(li);
+        found = (ptc->reference == object) ? true : false;
+    }
+    if (!found)
+        return false;
+
+    Object *extract = extract_after(li, previous);
+    return true;
+}
 
 void destroy_list(list_t *li)
 {
@@ -43,7 +63,7 @@ void insert_in_order(list_t *li, Object *object)
     }
     else
     {
-        element *ptc = li->first;
+        element_t *ptc = li->first;
         if (in_order(object, ptc->reference, li->type == 1, li->compare))
         {
             // insert before the first element
@@ -54,7 +74,7 @@ void insert_in_order(list_t *li, Object *object)
         {
             // printf("Insert in middle or tail of non empty list\n");
             boolean found = false;
-            element *previous = NULL;
+            element_t *previous = NULL;
             while (ptc != NULL && !found)
             {
                 previous = ptc;
